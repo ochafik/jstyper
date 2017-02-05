311ac5194ddb23d7a2ed95589a8e43496b5ded94
Olivier Chafik <olivier.chafik@gmail.com>
2017-02-05 11:58:48 +0000

    refactor indent

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,5 @@
 import {runTyper} from './typer';
+import {addTabIndentSupport} from './editor/indent';
 
 window.onload = () => {
   const jsInput = <HTMLTextAreaElement>document.getElementById('input');
@@ -20,7 +21,7 @@ function f(x, opts) {
     location.hash = '#' + encodeURIComponent(jsInput.value);
     autoRun();
   };
-  addTabSupport(jsInput);
+  addTabIndentSupport(jsInput);
   updateRunVisibility();
   autoRunCheckBox.onchange = () => {
     autoRun();
@@ -38,51 +39,11 @@ function f(x, opts) {
   function run() {
     stats.textContent = `Analyzing...`;
     const start = new Date().getTime();
-    const [[_, output]] = runTyper(new Map([['file.js', jsInput.value]]));
+    const result = runTyper(new Map([['file.js', jsInput.value]]))
+    const [[_, output]] = result;
     tsOutput.value = output;
     const time = new Date().getTime() - start;
     stats.textContent = `Took ${time} milliseconds`;
   }
   button.onclick = run;
 };
-
-function addTabSupport(textArea: HTMLTextAreaElement) {
-  const tab = '  ';
-  textArea.onkeydown = (e) => {
-    if ((e.keyCode || e.which) == '\t'.charCodeAt(0)) {
-        e.preventDefault();
-        const start = textArea.selectionStart;
-        const end = textArea.selectionEnd;
-        const text = textArea.value;
-        if (start == end && !e.shiftKey) {
-          textArea.value = text.substring(0, start) + tab + text.substring(end);
-          textArea.selectionStart = start + tab.length;
-          textArea.selectionEnd = textArea.selectionStart;
-        } else {
-          const selection = text.substring(start, end);
-          let preTabIndex = text.lastIndexOf('\n', start);  
-          if (preTabIndex < 0) preTabIndex = 0;
-          else preTabIndex++;
-
-          let skippedPreTabIndex = preTabIndex;
-          let newSelection: string;
-          let pre: string;
-          if (e.shiftKey) {
-            newSelection = selection.replace(new RegExp('\n' + tab, 'g'), '\n');
-            const i = text.indexOf(tab, preTabIndex);
-            if (text.indexOf(tab, preTabIndex) == preTabIndex) {
-              skippedPreTabIndex = preTabIndex + tab.length;
-            }
-            pre = '';
-          } else {
-            newSelection = selection.replace(/\n/g, '\n' + tab);
-            pre = tab;
-          }
-          textArea.value = text.substring(0, preTabIndex) + pre + text.substring(skippedPreTabIndex, start) + newSelection + text.substring(end);
-          const startOffset = pre.length + preTabIndex - skippedPreTabIndex;
-          textArea.selectionStart = start + startOffset;
-          textArea.selectionEnd = end + startOffset + newSelection.length - selection.length;
-        }
-    }
-  };
-}
