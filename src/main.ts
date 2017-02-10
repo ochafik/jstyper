5665f6f28c249e39c046e7f141f7a0cdb635bb66
Olivier Chafik <olivier.chafik@gmail.com>
2017-02-10 01:17:06 +0000

    Command line: -o d / --outputDir=d

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,57 @@
 import * as fs from "fs";
+import * as path from 'path';
 import {runTyper} from './typer';
 import {defaultOptions, Options} from './options';
 
-const fileNames = process.argv.slice(2);
+var argv = require('minimist')(process.argv.slice(2));
+// console.dir(argv);
+
+const outputDir = argv.outputDir || argv.o;
+
+const fileNames = argv['_'];
+
 const inputContents: {[fileName: string]: string} = Object.create(null);
 for (const fileName of fileNames) {
-  if (!fileName.endsWith('.js')) continue;
-//   if (fileName.indexOf('5') < 0) continue;
-
+  if (!fileName.endsWith('.js')) {
+    throw new Error(`File '${fileName}' does not have a JavaScript extension.`);
+  }
   const tsFileName = fileName.slice(0, -3) + '.ts';
   inputContents[tsFileName] = fs.readFileSync(fileName).toString();
 }
 
-const options = <Options>new Object(defaultOptions);
-options.currentWorkingDir = process.cwd();
-// options.maxIterations = 1;
+const options = <Options>{
+  ...defaultOptions,
+  currentWorkingDir: process.cwd(),
+  ...argv
+};
 const results = runTyper(inputContents, options);
 
 const metadataComment = `// ${results.metadata.inferencePasses} inference passes`;
     
 for (const fileName in results.outputs) {
     let content = results.outputs[fileName];
-    // console.warn(`${fileName}:`);
     // console.warn(content);
     
-    content += '\n\n' + metadataComment
+    // content += '\n\n' + metadataComment
     
     if (!fs.existsSync(fileName) || content != fs.readFileSync(fileName).toString()) {
-        fs.writeFileSync(fileName, content);
+        let outputFile = fileName;
+        if (outputDir) {
+          outputFile = path.join(outputDir, path.relative(options.currentWorkingDir, fileName));
+        }
+        mkdirsSync(path.dirname(outputFile));
+        
+        console.warn(`${path.relative(options.currentWorkingDir, outputFile)}: ${results.metadata.inferencePasses} inference passes`);
+        fs.writeFileSync(outputFile, content);
     }
+}
+
+function mkdirsSync(dir: string) {
+  dir.split(path.sep).reduce((d, component) => {
+    const dir = (d && d + '/' || '') + component;
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    return dir;
+  }, '');
 }
