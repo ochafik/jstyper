4a57455d526175bb5ea9a35aff1d40a633c7bac0
Olivier Chafik <olivier.chafik@gmail.com>
2017-02-10 00:38:50 +0000

    update specs

--- src/testing/support.ts
+++ src/testing/support.ts
@@ -4,37 +4,26 @@ import {runTyper} from '../typer';
 import {expect} from 'chai';
 import {TestSpec, writeSpec, readSpec} from './test_spec';
 
-const updateGoldens: boolean = process.env.UPDATE_GOLDENS == '1';
+const updateSpecs: boolean = process.env.UPDATE_SPECS == '1';
 
 export function typerTest(specFile: string): (this: mocha.ITestCallbackContext) => Promise<any>  {
   return async function() {
-    // console.warn(`${process.cwd()}`);
     const builtFile = process.cwd() + '/' + specFile.replace(/^(.*?)\.ts$/, 'build/$1.js');
     const spec = readSpec(builtFile);
-    // console.warn(`FOUND ${JSON.stringify(spec, null, 2)}`);
 
     const result = <TestSpec>runTyper(spec.inputs);
 
-    // const output = result.outputs[fileName];
-    // const annotatedOutput = `/*\n${input}\n*/\n${output}\n// ${result.metadata.inferencePasses} inference passes`;
     const actualSpec = {
       inputs: spec.inputs,
       outputs: result.outputs,
       metadata: result.metadata
     };
     
-    if (updateGoldens) {
+    if (updateSpecs) {
       await writeSpec(specFile, actualSpec);
-      // await writeFile(fileName, annotatedOutput);
     } else {
-      // if (!fs.existsSync(specFile)) {
-      //   throw new Error(`Golden file ${fileName} not found: update the tests with UPDATE_GOLDENS=1`);
-      // } else {
-
-        // const expected = (await readFile(fileName)).toString();
-        //expect(annotatedOutput).to.be.equal(expected);
-        assert.deepEqual(actualSpec, spec);
-      // }
+      //expect(annotatedOutput).to.be.equal(expected);
+      assert.deepEqual(actualSpec, spec);
     }
   }
 }
