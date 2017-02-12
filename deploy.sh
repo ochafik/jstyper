57ef86d571eb0ab6dee02d5cc73ecc02f919504c
Olivier Chafik <olivier.chafik@gmail.com>
2017-02-12 22:18:16 +0000

    update scripts

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -eu
 readonly TEMP_DIR="$PWD/tmp/ochafik.com"
 readonly RELATIVE_DEMO="assets/typer-demo.html"
 
-npm run prepare-release
+# npm run prepare-release
 
 rm -fR "${TEMP_DIR}"
 mkdir -p "${TEMP_DIR}"
